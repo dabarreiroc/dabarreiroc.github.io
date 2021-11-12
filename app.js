d18dafb456476d72ae5421ade22686b6aaabc7d5
dabarreiroc <djbarreiroc@gmail.com>
2021-11-12 23:29:45 +0100

    Full responsiveness implemented in: HOME, ABOUT, CONTACT.

--- app.js
+++ app.js
@@ -4,36 +4,22 @@ const navSlide = () => {
     const navLinks = document.querySelectorAll('.nav-links li');
 
     // togle nav
-    hamburger.addEventListener('click',() => {
+    hamburger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
         // animate links
         navLinks.forEach((link, index) => {
-            if (link.style.animation){
-            link.style.animation = ''
-             }
-            else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 1s ease forwards ${index / 9 + 1}s`;
             }
         });
 
         //close symbol after menu is out
-        hamburger.classList.toggle('toggle'); 
+        hamburger.classList.toggle('toggle');
     });
 }
 
 navSlide();
-
-
-const imgSlide = () => {
-    var counter = 1;
-    setInterval(function(){
-        document.getElementById('radio' + counter).checked = true;
-        counter ++;
-        if(counter > 3){
-            counter = 1;
-        }
-    },5000);
-}    
-
-imgSlide()
